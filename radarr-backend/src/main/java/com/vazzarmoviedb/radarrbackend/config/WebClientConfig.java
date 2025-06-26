f13ab4a864ea0f21a5bb6450ecb7e62f30bde88c
Vazul15 <magyar.vazul.zoltan@gmail.com>
2025-06-26 21:08:06 +0200

    webclientConfig created for cleancode purpose, indexerConfService under development

---
+++ radarr-backend/src/main/java/com/vazzarmoviedb/radarrbackend/config/WebClientConfig.java
@@ -0,0 +1,36 @@
+package com.vazzarmoviedb.radarrbackend.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.MediaType;
+import org.springframework.web.reactive.function.client.WebClient;
+
+
+
+@Configuration
+public class WebClientConfig {
+    @Value("${radarr.api.key}")
+    private String radarrApiKey;
+
+    @Value("${radarr.api.host}")
+    private String radarrApiHost;
+
+    @Value("${radarr.api.port}")
+    private int radarrApiPort;
+
+
+    @Bean
+    public WebClient radarrClient() {
+        String baseUrl = String.format("http://$s:$d/api/v3", radarrApiHost, radarrApiPort);
+
+        return WebClient.builder()
+                .baseUrl(baseUrl)
+                .defaultHeaders(headers -> {
+                    headers.setContentType(MediaType.APPLICATION_JSON);
+                    headers.set("X-Api-Key", radarrApiKey);
+                })
+                .build();
+    }
+
+}
--- radarr-backend/src/main/java/com/vazzarmoviedb/radarrbackend/service/IndexerConfService.java
+++ radarr-backend/src/main/java/com/vazzarmoviedb/radarrbackend/service/IndexerConfService.java
@@ -1,32 +1,25 @@
 package com.vazzarmoviedb.radarrbackend.service;
-
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.MediaType;
 import org.springframework.stereotype.Service;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.web.client.RestTemplate;
+import org.springframework.web.reactive.function.client.WebClient;
+import reactor.core.publisher.Mono;
+import java.util.List;
 
 @Service
 public class IndexerConfService {
 
-    @Value("${radarr.api.key}")
-    private String radarrApiKey;
+    private final WebClient radarrClient;
 
-    @Value("${radarr.api.host}")
-    private String radarrApiHost;
-
-    @Value("${radarr.api.port}")
-    private int radarrApiPort;
-
-    private String getRadarrIndexerBaseUrl(){
-        return "http://"+radarrApiHost+":"+radarrApiPort+"/api/v3/indexer";
+    public IndexerConfService(WebClient radarrClient) {
+        this.radarrClient = radarrClient;
     }
 
-    public void getIndexerr() {
-        RestTemplate restTemplate = new RestTemplate();
-        HttpHeaders headers = new HttpHeaders();
-        headers.set("X-API-KEY", radarrApiKey);
-        headers.setContentType(MediaType.APPLICATION_JSON);
-    }
 
+//    Indexer DTO thats what I will do when I will know what wil lbe exactly
+//    public Mono<List<IndexerDto>> getIndexers() {
+//        return radarrClient.get()
+//                .uri("indexer")
+//                .retrieve()
+//                .bodyToFlux(IndexerDto.class)
+//                .collectList();
+//    }
 }
